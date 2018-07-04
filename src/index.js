40bb11772dabd4db49f6fc4d894044589715d070
Anna Bagrowska <anna_bagrowska@wp.pl>
2018-07-04 17:50:58 +0200

    add routing

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
-import DevTools from './DevTools.js';
-import {getCountries} from './../src/actions/actions.js';
+import Navigation from './presentational/navigation.component';
 
 render(
     <Provider store={store}>
-    	<div>
-        	<h1>Inicjalizacja projektu</h1>
-        	<DevTools/>
-        </div>
+        <Router history={hashHistory}>
+            <Route path='/' component={Navigation}>
+
+            </Route>
+        </Router>
     </Provider>,
     document.getElementById('root')
 );
-
-store.dispatch(getCountries());
-
